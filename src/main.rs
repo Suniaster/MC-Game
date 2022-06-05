79e3af194fdd0093f1d89d24f5471425a9aaed71
Suniaster <thibousbr@gmail.com>
2022-06-05 00:02:05 -0300

    change systems to their own module

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-use voxelviewer::{ScreenView};
 use specs::prelude::*;
 
 mod entities;
@@ -37,75 +36,6 @@ mod components;
 
 use specs::DispatcherBuilder;
 
-struct RenderTextInfoSystem{
-    texts_ids: HashMap<&'static str, usize>,
-    time_counter: std::time::Duration,
-}
-
-impl<'a> System<'a> for RenderTextInfoSystem {
-    type SystemData = (
-        ReadExpect<'a, Arc<Mutex<ScreenView>>>,
-        Read<'a, WorldDt>,
-    );
-
-    fn run(&mut self, (scren_mutex, dt): Self::SystemData){
-        self.time_counter += dt.0;
-        if self.time_counter.as_secs_f32() > 0.1 {
-            self.time_counter = Duration::new(0, 0);
-            let mut screen = scren_mutex.lock().unwrap();
-            
-            let fps = 1./ dt.0.as_secs_f32();
-            let text = format!("Vextex count: {}", fps);
-            let id = self.texts_ids.get("fps").unwrap();
-            screen.actions.update_text(*id, text);
-
-            let looking_dir = screen.actions.state.camera.get_looking_dir();
-            let looking_text = format!("Looking XZ: ({}, {})", looking_dir[0], looking_dir[1]);
-            let id = self.texts_ids.get("looking").unwrap();
-            screen.actions.update_text(*id, looking_text);
-
-            let position = screen.actions.state.camera.get_position();
-            let position_text = format!(
-                "Position: ({:.0}, {:.0}, {:.0})", 
-                position.x, position.y, position.z
-            );
-            let id = self.texts_ids.get("position").unwrap();
-            screen.actions.update_text(*id, position_text);
-
-            let count = screen.actions.get_vertex_count();
-            let count_text = format!("Vertex count: {}", count);
-            let id = self.texts_ids.get("vertices").unwrap();
-            screen.actions.update_text(*id, count_text);
-        }
-    }
-
-    fn setup(&mut self, world: &mut World) {
-        Self::SystemData::setup(world);
-        let screen_mutex = world.read_resource::<Arc<Mutex<ScreenView>>>();
-        let mut screen = screen_mutex.lock().unwrap();
-
-        self.texts_ids.insert("fps", screen.actions.create_text());
-        self.texts_ids.insert("looking", screen.actions.create_text());
-        self.texts_ids.insert("position", screen.actions.create_text());
-        self.texts_ids.insert("vertices", screen.actions.create_text());
-    }
-}
-
-#[derive(Default)]
-struct WorldDt(Duration);
-struct UpdateDtSystem{
-    last_time: std::time::Instant
-}
-impl<'a> System<'a> for UpdateDtSystem {
-    type SystemData = Write<'a, WorldDt>;
-
-    fn run(&mut self, mut data: Self::SystemData) {
-        let now = std::time::Instant::now();
-        let dt = now.duration_since(self.last_time);
-        self.last_time = now;
-        data.0 = dt;
-    }
-}
 
 fn main() {
     let mut world = World::new();
@@ -118,17 +48,17 @@ fn main() {
     let arc_screen = Arc::new(Mutex::new(screen));
 
     let mut dispatcher = DispatcherBuilder::new()
-        .with(UpdateDtSystem{
+        .with(systems::UpdateDtSystem{
             last_time: std::time::Instant::now()
         }, "update_dt_system", &[])
-        .with_thread_local(RenderTextInfoSystem{
+        .with_thread_local(systems::RenderTextInfoSystem{
             texts_ids: HashMap::new(),
             time_counter: std::time::Duration::new(0, 0),
         })
         .build();
 
     world.insert(arc_screen.clone());
-    world.insert(WorldDt(Duration::new(0, 0)));
+    world.insert(systems::WorldDt(Duration::new(0, 0)));
 
     dispatcher.setup(&mut world);
     voxelviewer::start(world, dispatcher, arc_screen, evloop);
---
+++ src/systems/mod.rs
@@ -0,0 +1,76 @@
+use std::{collections::HashMap, sync::{Arc, Mutex}, time::Duration};
+
+use specs::prelude::*;
+use voxelviewer::ScreenView;
+
+pub struct RenderTextInfoSystem{
+    pub texts_ids: HashMap<&'static str, usize>,
+    pub time_counter: std::time::Duration,
+}
+
+impl<'a> System<'a> for RenderTextInfoSystem {
+    type SystemData = (
+        ReadExpect<'a, Arc<Mutex<ScreenView>>>,
+        Read<'a, WorldDt>,
+    );
+
+    fn run(&mut self, (scren_mutex, dt): Self::SystemData){
+        self.time_counter += dt.0;
+        if self.time_counter.as_secs_f32() > 0.1 {
+            self.time_counter = Duration::new(0, 0);
+            let mut screen = scren_mutex.lock().unwrap();
+            
+            let fps = 1./ dt.0.as_secs_f32();
+            let text = format!("Vextex count: {}", fps);
+            let id = self.texts_ids.get("fps").unwrap();
+            screen.actions.update_text(*id, text);
+
+            let looking_dir = screen.actions.state.camera.get_looking_dir();
+            let looking_text = format!("Looking XZ: ({}, {})", looking_dir[0], looking_dir[1]);
+            let id = self.texts_ids.get("looking").unwrap();
+            screen.actions.update_text(*id, looking_text);
+
+            let position = screen.actions.state.camera.get_position();
+            let position_text = format!(
+                "Position: ({:.0}, {:.0}, {:.0})", 
+                position.x, position.y, position.z
+            );
+            let id = self.texts_ids.get("position").unwrap();
+            screen.actions.update_text(*id, position_text);
+
+            let count = screen.actions.get_vertex_count();
+            let count_text = format!("Vertex count: {}", count);
+            let id = self.texts_ids.get("vertices").unwrap();
+            screen.actions.update_text(*id, count_text);
+        }
+    }
+
+    fn setup(&mut self, world: &mut World) {
+        Self::SystemData::setup(world);
+        let screen_mutex = world.read_resource::<Arc<Mutex<ScreenView>>>();
+        let mut screen = screen_mutex.lock().unwrap();
+
+        self.texts_ids.insert("fps", screen.actions.create_text());
+        self.texts_ids.insert("looking", screen.actions.create_text());
+        self.texts_ids.insert("position", screen.actions.create_text());
+        self.texts_ids.insert("vertices", screen.actions.create_text());
+    }
+}
+
+
+
+#[derive(Default)]
+pub struct WorldDt(pub Duration);
+pub struct UpdateDtSystem{
+    pub last_time: std::time::Instant
+}
+impl<'a> System<'a> for UpdateDtSystem {
+    type SystemData = Write<'a, WorldDt>;
+
+    fn run(&mut self, mut data: Self::SystemData) {
+        let now = std::time::Instant::now();
+        let dt = now.duration_since(self.last_time);
+        self.last_time = now;
+        data.0 = dt;
+    }
+}
