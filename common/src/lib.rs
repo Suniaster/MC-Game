50ce92a67c7a3fd8fb1c7e7523919bfbe5c0e34f
F9342353 Thiago Chaves Monteiro de Melo <thiago.chaves@bb.com.br>
2022-08-02 14:47:49 -0300

    Adds CommomComponentsPlugin

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,5 @@
 use nalgebra::Point3;
+use plugins::{Plugin, App};
 use specs::{Component, VecStorage};
 
 
@@ -6,3 +7,11 @@ use specs::{Component, VecStorage};
 pub struct PositionComponent(pub Point3<f32>);
 impl Component for PositionComponent {type Storage = VecStorage<Self>;}
 impl PositionComponent {pub fn new(position: Point3<f32>) -> Self {Self(position)}}
+
+
+pub struct CommonComponentsPlugin;
+impl Plugin for CommonComponentsPlugin {
+    fn build(&mut self, app: &mut App) {
+        app.add_component_storage::<PositionComponent>();
+    }
+}
